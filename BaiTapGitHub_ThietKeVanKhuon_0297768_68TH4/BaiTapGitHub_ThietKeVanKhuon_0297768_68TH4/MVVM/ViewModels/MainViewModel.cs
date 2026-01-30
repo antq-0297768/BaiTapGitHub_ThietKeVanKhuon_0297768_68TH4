038085eb5b5bfdd78d3970dee4838d2446f218b5
tranan15072005-ctrl <tranan15072005@gmail.com>
2026-01-30 10:03:14 +0700

    Test

---
+++ BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4.MVVM.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
---
+++ BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/MVVM/Views/MainView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
