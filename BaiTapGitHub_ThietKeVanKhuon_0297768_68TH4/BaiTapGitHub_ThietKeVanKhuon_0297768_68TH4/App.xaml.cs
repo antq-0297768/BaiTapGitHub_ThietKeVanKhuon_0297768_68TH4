bdfc87771c9c3d5f643e398c56777166935920b0
tranan15072005-ctrl <tranan15072005@gmail.com>
2026-01-30 10:03:14 +0700

    code LoginViewModel

---
+++ BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4/MVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapGitHub_ThietKeVanKhuon_0297768_68TH4.MVVM.ViewModels
+{
+    internal class LoginViewModel
+    {
+        public string title = "Login screen";
+    }
+}
